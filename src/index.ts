f0ce1e5114b26a6021818cd92f3887dca8bf84f4
linonetwo <linonetwo012@gmail.com>
2023-03-09 13:47:08 +0800

    feat: work

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,44 @@
-import type { IChangedTiddlers } from 'tiddlywiki';
+import type { IChangedTiddlers, IWidgetEvent } from 'tiddlywiki';
 import { widget as Widget } from '$:/core/modules/widgets/widget.js';
 
-class ExampleWidget extends Widget {
+class SpeechSynthesisWidget extends Widget {
   // constructor(parseTreeNode: IParseTreeNode, options?: unknown) {
   //   super(parseTreeNode, options);
   // }
 
-  refresh(_changedTiddlers: IChangedTiddlers): boolean {
-    return false;
+  refresh(changedTiddlers: IChangedTiddlers): boolean {
+    if ($tw.utils.count(this.computeAttributes()) > 0) {
+      this.refreshSelf();
+      return true;
+    }
+    return this.refreshChildren(changedTiddlers);
   }
 
   /**
    * Lifecycle method: Render this widget into the DOM
    */
-  render(parent: Element, _nextSibling: Element | null): void {
+  render(parent: Element, nextSibling: Element | null): void {
     this.parentDomNode = parent;
     this.computeAttributes();
     this.execute();
+    this.renderChildren(parent, nextSibling);
+  }
 
-    const containerElement = document.createElement('div');
-    containerElement.textContent = 'Hello world!';
-    this.domNodes.push(containerElement);
-    parent.appendChild(containerElement);
+  invokeAction(triggeringWidget: Widget, event: IWidgetEvent): boolean | undefined {
+    const text = this.getAttribute('text');
+    if (text) {
+      this.speak(text);
+    }
+    return;
+  }
+
+  private speak(text: string): void {
+    const msg = new SpeechSynthesisUtterance(text);
+    const voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[0];
+    window.speechSynthesis.speak(msg);
   }
 }
 
 declare const exports: Record<string, typeof Widget>;
-exports['example-widget'] = ExampleWidget;
+exports['speech-synthesis-widget'] = SpeechSynthesisWidget;
